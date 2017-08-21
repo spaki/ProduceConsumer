3266427d871f0738a8230353bab6ae6f4e5f356b
Eduardo Spaki <spoky.br@gmail.com>
2017-08-21 20:45:11 +0100

    first commit

---
+++ ProducerConsumer/ProducerConsumer/ItemHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Poc
+{
+    public class ItemHandler<T>
+    {
+        private List<Thread> consumerThreads = new List<Thread>();
+        private BlockingCollection<T> queue = new BlockingCollection<T>();
+        private CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+        private Action<T> processReceivingItem;
+
+        public ItemHandler(Action<T> processReceivingItem, int maxConsumerThreads = 2)
+        {
+            this.processReceivingItem = processReceivingItem;
+            this.StartConsumers(maxConsumerThreads);
+        }
+
+        public void AddItem(T item)
+        {
+            queue.Add(item);
+        }
+
+        public void Cancel()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+        private void StartConsumers(int maxConsumerThreads)
+        {
+            for (int i = 0; i < maxConsumerThreads; i++)
+            {
+                var consumerThread = new Thread(Consume);
+
+                consumerThreads.Add(consumerThread);
+
+                consumerThread.IsBackground = true;
+                consumerThread.Start();
+            }
+        }
+
+        private void Consume()
+        {
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                T item = queue.Take(cancellationTokenSource.Token);
+                processReceivingItem(item);
+            }
+        }
+    }
+}
---
+++ ProducerConsumer/ProducerConsumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Poc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var handler = new ItemHandler<Item>(DoThings);
+
+            Parallel.For(0, 1000, i => {
+                handler.AddItem(new Item { Id = Guid.NewGuid(), Number = i });
+            });
+
+            Console.WriteLine();
+            Console.WriteLine("end...");
+            Console.ReadLine();
+        }
+
+        static void DoThings(Item item)
+        {
+            Console.WriteLine($"item number {item.Number}, id {item.Id}");
+        }
+    }
+
+    class Item
+    {
+        public Guid Id { get; set; }
+
+        public int Number { get; set; }
+    }
+}
